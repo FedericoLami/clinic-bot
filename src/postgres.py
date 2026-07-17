24455fab034488aae5ff9af88ba61ed7f34c270e
Federico <federicolami@hotmail.com>
2026-07-17 03:24:45 -0300

    feat:agregar funciones restantes de postgres incluyendo calculo dinamico de turnos disponibles

--- src/postgres.py
+++ src/postgres.py
@@ -3,6 +3,7 @@ import os
 import random
 import string
 from dotenv import load_dotenv
+from datetime import datetime, timedelta, time
 
 load_dotenv()
 
@@ -41,3 +42,45 @@ def cancelar_turno(codigo_turno):
     pgCursor.execute("UPDATE turnos SET estado = 'cancelado' WHERE codigo_turno = %s ",(codigo_turno,))
     pgConnection.commit()
 
+def obtener_preguntas_frecuentes():
+    pgCursor.execute("SELECT * FROM preguntas_frecuentes")
+    resultados = pgCursor.fetchall()
+    return resultados
+
+def obtener_turnos_proximos_24hs():
+    pgCursor.execute("SELECT * FROM turnos WHERE estado = 'agendado' AND recordatorio_enviado = false AND (fecha + hora) BETWEEN NOW() AND NOW() + INTERVAL '24 hours'")
+    resultados = pgCursor.fetchall()
+    return resultados
+
+def marcar_recordatorio_enviado(idTurno):
+    pgCursor.execute("UPDATE turnos SET recordatorio_enviado = true WHERE idTurno = %s",(idTurno,))
+    pgConnection.commit()
+
+
+DIAS_LABORALES = [0, 1, 2, 4]  # 0=lunes, 1=martes, 2=miércoles, 4=viernes
+HORA_INICIO = time(10, 0)
+HORA_FIN = time(17, 0)
+INTERVALO_MINUTOS = 30
+
+def obtener_proximo_turno_disponible():
+    fecha_busqueda = datetime.now().date() + timedelta(days=1)
+    
+    for _ in range(365):
+        if fecha_busqueda.weekday() in DIAS_LABORALES:
+            
+            pgCursor.execute(
+                "SELECT hora FROM turnos WHERE fecha = %s AND estado = 'agendado'",
+                (fecha_busqueda,)
+            )
+            horas_ocupadas = [row[0] for row in pgCursor.fetchall()]
+            
+            slot_actual = datetime.combine(fecha_busqueda, HORA_INICIO)
+            fin_dia = datetime.combine(fecha_busqueda, HORA_FIN)
+            
+            while slot_actual < fin_dia:
+                if slot_actual.time() not in horas_ocupadas:
+                    return {"fecha": fecha_busqueda, "hora": slot_actual.time()}
+                slot_actual += timedelta(minutes=INTERVALO_MINUTOS)
+        
+        fecha_busqueda += timedelta(days=1)
+    return None
